1a54af4670e21074804b2cba32d94dad4f9ffe7f
Benjamin Kosten <benkosten@gmail.com>
2017-10-20 00:30:19 -0400

    Add initial salem game?
    Add initial discord bot

---
+++ server/bot.py
@@ -0,0 +1,84 @@
+import discord
+from discord.ext import commands
+import random
+import time
+
+description = "An example bot to showcase the discord.ext.commands extension module."
+
+bot = commands.Bot(command_prefix='!', description=description)
+
+deaths = {}
+users = {}
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('-' * len(bot.user.id))
+
+@bot.command()
+async def rawr():
+    await bot.say("XD")
+
+@bot.command()
+async def kill(name: str, *argv: str):
+    author = commands.bot._get_variable('_internal_author')
+
+    if author not in users:
+        users[author] = time.time() + 86500
+
+    if users[author] - time.time() >= 86400:
+        for i in argv:
+            name = name + " " + i
+
+        if name not in deaths:
+            deaths[name] = 0
+
+        deaths[name] += 1
+        users[author] = time.time()
+        await bot.say(name + " has died " + str(deaths[name]) + " time(s)")
+
+    else:
+        await bot.say("Don't be a greedy bastard, wait " + time.strftime('%H:%M:%S', time.gmtime(86400 - (time.time() - users[author]))))
+
+bot.run('MzcwMDQ3MzI5NTYwMTY2NDAz.DMhb2Q.MTiThgXNKYlSH0pWieXOSarTX-0')
+
+"""
+@bot.command()
+async def add(left : int, right : int):
+    await bot.say(left + right)
+
+@bot.command()
+async def roll(dice : str):
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await bot.say('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await bot.say(result)
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(*choices : str):
+    await bot.say(random.choice(choices))
+
+@bot.command()
+async def repeat(times : int, content='repeating...'):
+    for i in range(times):
+        await bot.say(content)
+
+@bot.command()
+async def joined(member : discord.Member):
+    await bot.say('{0.name} joined in {0.joined_at}'.format(member))
+
+@bot.group(pass_context=True)
+async def cool(ctx):
+    if ctx.invoked_subcommand is None:
+        await bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
+
+@cool.command(name='bot')
+async def _bot():
+    await bot.say('Yes, the bot is cool.')
+"""
---
+++ server/salem.py
@@ -0,0 +1,36 @@
+import uuid
+import asyncio
+
+class Player:
+    def __init__(self, p_id, name):
+        self.id = p_id
+        self.name = name
+        self.alive = False
+
+class Game:
+    def __init__(self):
+        self.game_id = uuid.uuid4()
+        self.players = []
+        self.deceased = []
+        self.player_count = len(self.players)
+        self.alive_count = self.player_count
+        self.day = 1
+        self.night = False
+
+    async def add_player(self):
+        pass
+
+    async def kill(self, player):
+        self.players.remove(player)
+        self.deceased.append(player)
+
+    async def new_day(self):
+        self.day += 1
+
+    async def night(self):
+        self.night = True
+
+    async def day(self):
+        self.night = False
+
+g = Game()
